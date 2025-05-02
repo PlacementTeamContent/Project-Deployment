32b0ccf5c5a7606d120815331e748879af29d6c6
PST - Content <139737866+PlacementTeamContent@users.noreply.github.com>
2025-05-02 17:48:47 +0530

    Delete April 2025/3 directory

--- April 2025/3/b59d0347-5b8c-4b33-afdc-d160f5782e98/script.js
+++
@@ -1,7 +0,0 @@
-let nameEl = document.getElementById("name");
-let fatherNameEl = document.getElementById("father-name");
-let emailEl = document.getElementById("email");
-let phoneEl = document.getElementById("phone");
-let altPhoneEl = document.getElementById("alt-phone");
-let sscEl = document.getElementById("ssc");
-let nameEl = document.getElementById("name");
